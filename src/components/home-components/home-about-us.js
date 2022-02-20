f38cd0b48faf1c87fc694fe3f9cca09a5528350f
Jaith Kaushalya <jaithkaushalya99@gmail.com>
2022-02-20 09:42:05 +0530

    Adding make request UI

---
+++ src/components/home-components/home-about-us.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import { Container, Grid, Card, CardContent, CardMedia } from '@mui/material';
+
+import aboutUs from "../../assets/images/aboutUs.png";
+
+import SvgIcon from '@mui/material/SvgIcon';
+import Icon from '@mui/material/Icon';
+
+
+function AboutUs() {
+
+    return (
+        <div>
+            <Container><br /><br /><br /><br /><br /><br />
+                <Grid container spacing={4}>
+                    <Grid item xs={6}>
+
+                        <Box
+                            component="img"
+                            sx={{
+                                height: 380,
+                                display: 'block',
+                                //   maxWidth: '1500',
+                                overflow: 'hidden',
+                                width: '100%',
+                            }}
+                            src={aboutUs}
+                            alt={"aboutUs"}
+                        />
+
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="h5" gutterBottom component="div">
+                            About Us
+                        </Typography>
+                        <Typography variant="h6" gutterBottom component="div">
+                            We are Pick {"&"} Go Deliveries
+                        </Typography><br />
+                        <Typography variant="body2" gutterBottom>
+                            body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                            blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+                            neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+                            quasi quidem quibusdam.<br/><br/>
+
+                            body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                            blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+                            neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+                            quasi quidem quibusdam.body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                            blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+                            neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+                            quasi quidem quibusdam.
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+
+    );
+}
+
+export default AboutUs;
